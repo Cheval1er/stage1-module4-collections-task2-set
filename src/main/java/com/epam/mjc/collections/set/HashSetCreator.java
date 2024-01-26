51d4cbe0c97859d8207a7261af1337a14938595b
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2024-01-26 17:24:46 +0400

    Set Interface Tasks

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.set;
-
 import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (Integer x : sourceList) {
+            while (x % 2 == 0){
+                hashSet.add(x);
+                x = x / 2;
+            }
+            hashSet.add(x);
+            hashSet.add(2*x);
+        }
+        return hashSet;
     }
 }
